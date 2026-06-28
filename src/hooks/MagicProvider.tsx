8191ffe3161ac9e9f8d38f47dbffce1ef6a7713c
Falco90 <falcorodenburg@gmail.com>
2026-06-28 21:29:01 +0700

    wrap app with MagicProvider and UniversalAccountProvider, basic email login flow setup

---
+++ src/hooks/MagicProvider.tsx
@@ -0,0 +1,44 @@
+import { EVMExtension } from '@magic-ext/evm';
+import { Magic as MagicBase } from 'magic-sdk';
+import { ReactNode, createContext, useContext, useEffect, useMemo, useState } from 'react';
+
+export type Magic = MagicBase<[EVMExtension]>;
+
+type MagicContextType = {
+  magic: Magic | null;
+};
+
+const MagicContext = createContext<MagicContextType>({
+  magic: null,
+});
+
+export const useMagic = () => useContext(MagicContext);
+
+const MagicProvider = ({ children }: { children: ReactNode }) => {
+  const [magic, setMagic] = useState<Magic | null>(null);
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_MAGIC_API_KEY) {
+      const magic = new MagicBase(process.env.NEXT_PUBLIC_MAGIC_API_KEY as string, {
+        extensions: [
+          new EVMExtension([
+            { rpcUrl: process.env.NEXT_PUBLIC_BASE_RPC_URL || 'https://mainnet.base.org', chainId: 8453, default: true },
+          ]),
+        ],
+      });
+
+      setMagic(magic);
+    }
+  }, []);
+
+  const value = useMemo(() => {
+    return {
+      magic,
+    };
+  }, [magic]);
+
+  return <MagicContext.Provider value={value}>{children}</MagicContext.Provider>;
+};
+
+export default MagicProvider;
+
---
+++ src/utils/types.ts
@@ -0,0 +1,9 @@
+import { Dispatch, SetStateAction } from 'react'
+
+export type LoginProps = {
+  token: string
+  setToken: Dispatch<SetStateAction<string>>
+}
+
+export type { Magic } from '../hooks/MagicProvider'
+
